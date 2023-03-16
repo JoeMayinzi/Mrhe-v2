862b2d1c30b246cf34cba6c7eec935b82691b30c
JoeMayinzi <joemayinzi@gmail.com>
2023-03-16 20:11:44 +0100

    fixing sidebar

--- js/main.js
+++ js/main.js
@@ -3,21 +3,18 @@ const contactButton = document.getElementById("contactButton");
 
 contactButton.addEventListener("click", (Event) => {
   Event.preventDefault();
-  console.log("prevent contact loading");
 });
 
 newsletterButton.addEventListener("click", (Event) => {
   Event.preventDefault();
-  console.log("prevent page loading on click");
 });
 
 function openNav() {
   document.getElementById("mySidenav").style.width = 100 + "%";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.body.style.overflowY = "hidden";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("mySidenav").style.width = 50 + "%";
 }
