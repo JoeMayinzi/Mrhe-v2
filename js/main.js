01a51cca776bac8ea07b570e01ba89f006dc1b6d
JoeMayinzi <joemayinzi@gmail.com>
2023-03-22 11:18:09 +0100

    fix : fixing sidebar issue

--- js/main.js
+++ js/main.js
@@ -17,6 +17,6 @@ function openNav() {
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.body.style.overflowY = "visible";
+  document.getElementById("mySidenav").style.width = 0;
+  console.log("close nav");
 }
